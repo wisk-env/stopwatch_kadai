5f7e3a620373593b75da7a4d7fe3c98469799508
EC2 Default User <ec2-user@ip-172-31-12-71.ap-southeast-2.compute.internal>
2023-03-26 09:41:09 +0000

    ストップウォッチ計測機能の作成

--- main.js
+++ main.js
@@ -10,8 +10,51 @@ $(document).ready(function(){
   });
   
   $(".reset-button").click(function() {
-    $(this).prop("disabled", true)
+    $(".reset-button, .stop-button").prop("disabled", true)
     $(".start-button").prop("disabled", false)
+    $(".reset-number").text("00:00:00:00")
   })
 });
 
+let time = 0;
+let hour = 0;
+let min = 0;
+let sec = 0;
+let miliSec = 0;
+ 
+function startCountEvent() {
+  time = setInterval("countUp()", 100);
+}
+
+function countUp() {
+  miliSec ++;
+  if (miliSec == 10) {
+    miliSec = 0;
+    sec++;
+  } else if (sec == 60) {
+    sec = 0;
+    min++;
+  } else if (min == 60) {
+    min = 0;
+    hour++;
+  }
+
+  const hourStr = hour.toString().padStart(2, '0');
+  const minStr = min.toString().padStart(2, '0');
+  const secStr = sec.toString().padStart(2, '0');
+  const miliSecStr = miliSec.toString().padStart(2, '0');
+
+  document.getElementById('stop-watch').innerHTML = hourStr + ":" + minStr + ":" + secStr + ":" + miliSecStr;
+}
+
+function stopCountEvent() {
+  clearInterval(time);
+}
+
+function resetCountEvent() {
+  clearInterval(time);
+  hour = 0;
+  min = 0;
+  sec = 0;
+  miliSec = 0;
+}
